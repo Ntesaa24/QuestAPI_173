24ba4874c382a132968b32fe8794a22339646277
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-12-26 21:42:05 +0700

    memperbaiki DetailViewModel.kt

--- app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.pertemuan12.viewmodel
 
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.modeldata.DataSiswa
+import com.example.pertemuan12.repositori.RepositoryDataSiswa
+import com.example.pertemuan12.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+	data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+	object Error : StatusUIDetail
+	object Loading : StatusUIDetail
+}
+
