93ef18dbfd0ee172720a9be36951fa70c94bd778
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-12-26 13:59:12 +0700

    menambahkan OVERIDE pada DestinasiEdit.kt

--- app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEdit.kt
@@ -3,5 +3,6 @@ package com.example.pertemuan12.uicontroller.route
 import com.example.pertemuan12.R
 
 object DestinasiEdit: DestinasiNavigasi {
-
+	override val route = "item_edit"
+	override val titleRes = R.string.edit_siswa
 }
